35cc1d2659e5f618d4a2c2a2a09a306312fbd224
Ihar-Batura <A17331582@gmail.com>
2024-01-17 13:53:38 +0300

    feat: add code to 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,33 @@
+//Import all required modules.
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
+//Creat a text file
+fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
+  if (err) {
+    throw err;
+  }
+});
 
-const start = node;
+//Display a welcome message in the console.
+process.stdout.write('Hi, \nCan you write something interesting\n');
 
+//Wait for user input, write the entered text to the file.
+process.stdin.on('data', (data) => {
+  fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+  //Checking for the presence of the keyword `exit`.
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  }
+});
 
-console.log(start);
-
-2 + 3;
-
-
-console.log('.exit');
+//A farewell message when the process is stopped.
+process.on('exit', () => console.log('Bye,bye \nSee you later!'));
+process.on('SIGINT', () => {
+  process.exit();
+});
