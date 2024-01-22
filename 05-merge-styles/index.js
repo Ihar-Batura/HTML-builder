f91749454e496253ab2e2e0115b0a3d7fd3eac14
Ihar-Batura <A17331582@gmail.com>
2024-01-22 16:57:37 +0300

    fix: fix file in 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,15 +5,24 @@ fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '', (err) => {
   if (err) throw err;
 });
 fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
-    if (err) throw err;
-    for (let file of files) {
-        if (path.extname(file) === '.css') {
-            fs.readFile(path.join(__dirname, 'styles', file), 'utf-8', function(err, fileContent) {
-                if (err) throw err;
-                fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), fileContent, (err) => {
-                    if (err) throw err;
-                })
-            })
-        }
+  if (err) throw err;
+  for (let file of files) {
+    if (path.extname(file) === '.css') {
+      //Перезаписывает только один файл, нужно допилить!
+      fs.readFile(
+        path.join(__dirname, 'styles', file),
+        'utf-8',
+        function (err, fileContent) {
+          if (err) throw err;
+          fs.writeFile(
+            path.join(__dirname, 'project-dist', 'bundle.css'),
+            fileContent,
+            (err) => {
+              if (err) throw err;
+            },
+          );
+        },
+      );
     }
+  }
 });
