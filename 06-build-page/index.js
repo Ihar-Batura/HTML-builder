c84ed0418deeac6b2acda44b6d5da3c0036279ad
Ihar-Batura <A17331582@gmail.com>
2024-01-23 15:21:20 +0300

    feat: add code for copy files from folders img and svg

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,25 +7,25 @@ fs.mkdir(path.join(__dirname, 'project-dist'), (err) => {
 });
 // Создает папку assets
 fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), (err) => {
-    if (err) console.log('folder project-dist/assets already exist');
-  });
+  if (err) console.log('folder project-dist/assets already exist');
+});
 // Создает папку assets/fonts
 fs.mkdir(path.join(__dirname, 'project-dist', 'assets', 'fonts'), (err) => {
-    if (err) console.log('folder assets/fonts already exist');
-  });
+  if (err) console.log('folder assets/fonts already exist');
+});
 // Создает папку assets/img
 fs.mkdir(path.join(__dirname, 'project-dist', 'assets', 'img'), (err) => {
-    if (err) console.log('folder assets/img already exist');
-  });
+  if (err) console.log('folder assets/img already exist');
+});
 // Создает папку assets/svg
 fs.mkdir(path.join(__dirname, 'project-dist', 'assets', 'svg'), (err) => {
-    if (err) console.log('folder assets/svg already exist');
-  });
+  if (err) console.log('folder assets/svg already exist');
+});
 
 //Создает файл index.html
 fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), '', (err) => {
-    if (err) throw err;
-  });
+  if (err) throw err;
+});
 //Создает файл style.css
 fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (err) => {
   if (err) throw err;
@@ -53,26 +53,100 @@ fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
 });
 
 //Удаляет файлы из папки fonts
-fs.readdir(path.join(__dirname,'project-dist', 'assets', 'fonts'), (err, files) => {
+fs.readdir(
+  path.join(__dirname, 'project-dist', 'assets', 'fonts'),
+  (err, files) => {
     if (err) throw err;
     for (let file of files) {
-      const link = path.join(__dirname, 'project-dist', 'assets', 'fonts', file);
+      const link = path.join(
+        __dirname,
+        'project-dist',
+        'assets',
+        'fonts',
+        file,
+      );
       fs.unlink(link, (err) => {
         if (err) throw err;
       });
     }
-  });
+  },
+);
 //Копирует файлы из папки fonts в project-dist/assets/fonts
-  fs.readdir(path.join(__dirname, 'assets', 'fonts'), (err, files) => {
+fs.readdir(path.join(__dirname, 'assets', 'fonts'), (err, files) => {
+  if (err) throw err;
+  for (let file of files) {
+    const originalFile = path.join(__dirname, 'assets', 'fonts', file);
+    const copyFile = path.join(
+      __dirname,
+      'project-dist',
+      'assets',
+      'fonts',
+      file,
+    );
+    fs.copyFile(originalFile, copyFile, (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+//Удаляет файлы из папки img
+fs.readdir(
+  path.join(__dirname, 'project-dist', 'assets', 'img'),
+  (err, files) => {
     if (err) throw err;
     for (let file of files) {
-      const originalFile = path.join(__dirname, 'assets', 'fonts', file);
-      const copyFile = path.join(__dirname, 'project-dist', 'assets', 'fonts', file);
-      fs.copyFile(originalFile, copyFile, (err) => {
+      const link = path.join(__dirname, 'project-dist', 'assets', 'img', file);
+      fs.unlink(link, (err) => {
         if (err) throw err;
       });
     }
-  });
+  },
+);
+//Копирует файлы из папки fonts в project-dist/assets/img
+fs.readdir(path.join(__dirname, 'assets', 'img'), (err, files) => {
+  if (err) throw err;
+  for (let file of files) {
+    const originalFile = path.join(__dirname, 'assets', 'img', file);
+    const copyFile = path.join(
+      __dirname,
+      'project-dist',
+      'assets',
+      'img',
+      file,
+    );
+    fs.copyFile(originalFile, copyFile, (err) => {
+      if (err) throw err;
+    });
+  }
+});
 
-
-
+//Удаляет файлы из папки svg
+fs.readdir(
+  path.join(__dirname, 'project-dist', 'assets', 'svg'),
+  (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      const link = path.join(__dirname, 'project-dist', 'assets', 'svg', file);
+      fs.unlink(link, (err) => {
+        if (err) throw err;
+      });
+    }
+  },
+);
+//Копирует файлы из папки fonts в project-dist/assets/svg
+fs.readdir(path.join(__dirname, 'assets', 'svg'), (err, files) => {
+  if (err) throw err;
+  for (let file of files) {
+    const originalFile = path.join(__dirname, 'assets', 'svg', file);
+    const copyFile = path.join(
+      __dirname,
+      'project-dist',
+      'assets',
+      'svg',
+      file,
+    );
+    fs.copyFile(originalFile, copyFile, (err) => {
+      if (err) throw err;
+    });
+  }
+});
