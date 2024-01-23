7b94a6629dacf14b6e18996225e4be30f7a25569
Ihar-Batura <A17331582@gmail.com>
2024-01-23 13:07:42 +0300

    feat: add second part of code to 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,9 +2,37 @@ const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(path.join(__dirname, 'project-dist'), (err) => {
-  if (err) console.log('folder files-copy already exist');
+  if (err) console.log('folder project-dist already exist');
 });
+fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), (err) => {
+    if (err) console.log('folder project-dist/assets already exist');
+  });
 
-fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (err) => {
+fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), '', (err) => {
     if (err) throw err;
   });
+
+fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+  if (err) throw err;
+  for (let file of files) {
+    //Перезаписывает только один файл, нужно допилить!
+    fs.readFile(
+      path.join(__dirname, 'styles', file),
+      'utf-8',
+      function (err, fileContent) {
+        if (err) throw err;
+        fs.writeFile(
+          path.join(__dirname, 'project-dist', 'style.css'),
+          fileContent,
+          (err) => {
+            if (err) throw err;
+          },
+        );
+      },
+    );
+  }
+});
